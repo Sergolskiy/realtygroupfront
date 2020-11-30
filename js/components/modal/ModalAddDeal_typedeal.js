a1fff250b4e19e83d337d537b51be9b33778e75a
DarthJktu <DarthJktu@gmail.com>
2020-11-30 17:44:49 +0200

    initial

--- js/components/modal/ModalAddDeal_typedeal.js
+++ js/components/modal/ModalAddDeal_typedeal.js
@@ -2,25 +2,41 @@ import React from 'react'
 
 export class ModalAddDeal_typedeal extends React.Component {
 
-    render() {
+  render() {
 
-        const {clickTypeDeal, dealtype, dealdirection} = this.props;
+    const {clickTypeDeal, dealtype, dealdirection} = this.props;
 
-        return (
-            <div className="modal-dealtype-btns">
-                <div className="mb-2">
-                    <div className={"btn-choose _typedeal mr-2" + ((dealtype === 'rent' && dealdirection === 'request') ? ' active' : '')}
-                         onClick={() => clickTypeDeal('rent', 'request')}>Сниму</div>
-                    <div className={"btn-choose _typedeal" + ((dealtype === 'rent' && dealdirection === 'object') ? ' active' : '')}
-                         onClick={() => clickTypeDeal('rent', 'object')}>Сдам</div>
-                </div>
-                <div>
-                    <div className={"btn-choose _typedeal mr-2" + ((dealtype === 'sale' && dealdirection === 'request') ? ' active' : '')}
-                         onClick={() => clickTypeDeal('sale', 'request')}>Куплю</div>
-                    <div className={"btn-choose _typedeal" + ((dealtype === 'sale' && dealdirection === 'object') ? ' active' : '')}
-                         onClick={() => clickTypeDeal('sale', 'object')}>Продам</div>
-                </div>
-            </div>
-        )
-    }
+    let role = JSON.parse(window.localStorage.getItem('access')).role;
+
+    return (
+      <div className="modal-dealtype-btns">
+
+        {role === 'ROLE_ADMIN' || role === 'ROLE_REALTOR_RENT' || role === 'ROLE_REALTOR_SALE_RENT' &&
+        <div className="mb-2">
+          <div
+            className={"btn-choose _typedeal mr-2" + ((dealtype === 'rent' && dealdirection === 'request') ? ' active' : '')}
+            onClick={() => clickTypeDeal('rent', 'request')}>Сниму
+          </div>
+          <div
+            className={"btn-choose _typedeal" + ((dealtype === 'rent' && dealdirection === 'object') ? ' active' : '')}
+            onClick={() => clickTypeDeal('rent', 'object')}>Сдам
+          </div>
+        </div>
+        }
+
+        {role === 'ROLE_ADMIN' || role === 'ROLE_REALTOR_SALE' || role === 'ROLE_REALTOR_SALE_RENT' &&
+        <div>
+          <div
+            className={"btn-choose _typedeal mr-2" + ((dealtype === 'sale' && dealdirection === 'request') ? ' active' : '')}
+            onClick={() => clickTypeDeal('sale', 'request')}>Куплю
+          </div>
+          <div
+            className={"btn-choose _typedeal" + ((dealtype === 'sale' && dealdirection === 'object') ? ' active' : '')}
+            onClick={() => clickTypeDeal('sale', 'object')}>Продам
+          </div>
+        </div>
+        < /div>
+        }
+    )
+  }
 }
